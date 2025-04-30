0484c5d5012980f6b72526afc05949c8025c75ea
juju.souza09@yahoo.com <juju.souza09@yahoo.com>
2025-04-30 14:59:44 -0300

    Aula 13

--- CorridaMaluca.java
+++ CorridaMaluca.java
@@ -1,5 +1,51 @@
 package Aula13;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class CorridaMaluca {
+	
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		Random random = new Random();
+		
+		String corredor1, corredor2, corredor3;
+		int posicao1 = 0;
+		int posicao2 = 0;
+		int posicao3 = 0;
+		
+		System.out.println("Bem-vindo a Corrida Maluca!");
+		System.out.println("Informe o nome do 1º corredor: ");
+		corredor1 = ler.next();
+		
+		System.out.println("Informe o nome do 2º corredor: ");
+		corredor2 = ler.next();
+		
+		System.out.println("Informe o nome do 3º corredor: ");
+		corredor3 = ler.next();
+		
+		for (int i = 1; i <= 5; i++) {
+			posicao1 += random.nextInt(3)+1;
+			posicao2 += random.nextInt(3)+1;
+			posicao3 += random.nextInt(3)+1;
+			
+		}
+		
+		if (posicao1 < posicao2 && posicao1 < posicao3) {
+			System.out.println("O corredor " + corredor1 + " é o vencedor!");
+		}
+		
+		else if (posicao2 < posicao1 && posicao2 < posicao3) {
+			System.out.println("O corredor " + corredor2 + " é o vencedor!");
+		}
+		
+		else {
+			System.out.println("O corredor " + corredor3 + " é o vencedor!");
+		}
+		
+		ler.close();
+		
+	}
 
 }
--- NumerosMagicos.java
+++ NumerosMagicos.java
@@ -1,6 +1,5 @@
 package Aula13;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class NumerosMagicos {
@@ -8,31 +7,34 @@ public class NumerosMagicos {
 	public static void main(String[] args) {
 		
 		//DECLARANDO VARIAVEIS
-		int numeroMagico;
-		int escolha;
-		int acertos = 0;
-		int erros = 0;
+		int num;
+		int primo = 1;
 		
 		Scanner ler = new Scanner(System.in);
-		Random random = new Random();
+		System.out.println("Informe o número para descobrir se é primo ou não: ");
+		num = ler.nextInt();
 		
-		for (int i = 1; i <= 5; i++) {
-			
-			numeroMagico = random.nextInt(50);
-			
-		System.out.println("Tente adivinhar o número: ");
-		escolha = ler.nextInt();
+		if (num <= 1) {
+			primo = 0;
 		}
 		
-		if (escolha == numeroMagico) {
-			System.out.println("Parabéns, você acertou!");
-			acertos++;
-		} else  {
-			System.out.println("Não foi dessa vez!");
-			erros++;
+		else {
+			for (int i = 2; i < num; i++) {
+				if (num % i == 0) {
+					primo = 0;
+					break;
+				}
+			}
 		}
-	
-		System.out.println("Acertos " + acertos);
-		System.out.println("Erros " + erros);
+		
+		if (primo == 1) {
+			System.out.println(num + " é um número primo. ");
+		}
+		else {
+			System.out.println(num + " não é um número primo. ");
+		}
+		
+		ler.close();
 	}
+
 }
